a0c2f51dd85bfc4848311aa4cb6ec6a0749fc297
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 22:44:15 +0300

    add transaction caching

--- app/Account.php
+++ app/Account.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use App\Helpers\Transaction\TransactionDTO;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Account extends Model
 {
@@ -10,4 +12,28 @@ class Account extends Model
         'name',
         'balance'
     ];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'from');
+    }
+
+    public function getCahceVariable()
+    {
+        return "transaction_of_" . $this->id;
+    }
+
+    public function cacheTransactions()
+    {
+        $transactionDTOs = array_map(function (array $transaction) {
+            return new TransactionDTO(
+                $transaction['id'],
+                $transaction['from'],
+                $transaction['to'],
+                floatval($transaction['amount']),
+                $transaction['details']
+            );
+        }, $this->transactions->toArray());
+        Cache::put($this->getCahceVariable(), $transactionDTOs);
+    }
 }
