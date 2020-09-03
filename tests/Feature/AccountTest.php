6d717cb5df3547370a09f820a864c96bbac73e7c
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 20:52:15 +0300

    create test for account not found

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Account;
+use App\Helpers\Account\AccountFacade;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -23,5 +24,15 @@ class AccountTest extends TestCase
         $response = $this->json('GET', route('accounts.show', ['account' => $account->id]));
         $response->assertStatus(200);
         $response->assertJson(['success' => true]);
+        $response->assertJson(['data' => ['account' => AccountFacade::getDTO($account)->toArray()]]);
+    }
+
+    /**
+     * @test
+     */
+    public function testAccountNotFound()
+    {
+        $response = $this->json('GET', route('accounts.show', ['account' => 1]));
+        $response->assertStatus(404);
     }
 }
