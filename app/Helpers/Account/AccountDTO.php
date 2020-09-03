e0d02f6f0713143ac5f013df8321eb378b85c4fe
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 20:47:36 +0300

    create AccountDTO

---
+++ app/Helpers/Account/AccountDTO.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers\Account;
+
+class AccountDTO
+{
+    private $accountId;
+    private $name;
+    private $balance;
+
+    function __construct(int $accountId, string $name, float $balance)
+    {
+        $this->accountId = $accountId;
+        $this->name = $name;
+        $this->balance = $balance;
+    }
+
+    /**
+     * Get the value of accountId
+     */
+    public function getAccountId(): int
+    {
+        return $this->accountId;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of balance
+     */
+    public function getBalance(): float
+    {
+        return $this->balance;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->getAccountId(),
+            'name' => $this->getName(),
+            'balance' => $this->getBalance()
+        ];
+    }
+}
