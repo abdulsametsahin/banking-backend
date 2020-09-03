7dda324c5b7376724fc4a1c924159d2735cfbdec
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 22:09:36 +0300

    create transaction store route

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,5 @@
 
 Route::prefix('accounts')->group(function () {
     Route::get('{account}', 'AccountController@show')->name('accounts.show');
+    Route::post('{account}/transactions', 'TransactionController@store')->name('transactions.store');
 });
