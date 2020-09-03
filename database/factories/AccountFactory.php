bf1442d06478236fc8f15c490dec153a696161f4
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 22:10:09 +0300

    fix: min balance should be bigger than zero

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Account::class, function (Faker $faker) {
     return [
         'name' => $faker->name(),
-        'balance' => $faker->randomFloat(3, 0, 999999)
+        'balance' => $faker->randomFloat(3, 10000, 999999)
     ];
 });
