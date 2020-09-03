6f707f1296c51052bc2cf3a067ff415172b195b0
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 21:15:00 +0300

    create TransactionFactory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Account;
+use App\Transaction;
+use Faker\Generator as Faker;
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    $from = Account::select('id')
+        ->inRandomOrder()
+        ->first();
+    $to = Account::select('id')
+        ->inRandomOrder()
+        ->where('id', '!=', $from->id)
+        ->first();
+
+    return [
+        'from' => $from->id,
+        'to' => $to->id,
+        'amount' => $faker->randomFloat(3, 0, 999999),
+        'details' => $faker->text(250)
+    ];
+});
