bf0be4c55481e20259f0991b8fd1a5872c4e7ca4
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 22:44:23 +0300

    create show method

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Account;
+use App\Helpers\Transaction\TransactionDTO;
 use App\Helpers\Transaction\TransactionFacade;
 use App\Http\Requests\TransactionStoreRequest;
 use Illuminate\Http\JsonResponse;
 
 class TransactionController extends Controller
 {
+    public function show(Account $account)
+    {
+        $transactions = array_map(function (TransactionDTO $transactionDTO) {
+            return $transactionDTO->toArray();
+        }, TransactionFacade::get($account));
+
+        return $this->makeResponse(compact('transactions'));
+    }
+
     public function store(TransactionStoreRequest $request): JsonResponse
     {
         return TransactionFacade::save($request->denormalize()->toTransactionDTO());
