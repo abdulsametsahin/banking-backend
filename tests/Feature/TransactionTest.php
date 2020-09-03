136db8947c81dc20dc0f76f26722325ade931248
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 23:11:14 +0300

    test overBalance and gettingTransactions

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -26,7 +26,41 @@ class TransactionTest extends TestCase
         ];
 
         $response = $this->json('POST', route('transactions.store', ['account', 1]), $data);
-        $response->assertStatus(200);
+        $response->assertStatus(201);
         $response->assertJson(['success' => true]);
+    }
+
+    public function testMakeTransactionOverBalance()
+    {
+        factory(Account::class, 2)->create();
+
+        $data = [
+            "from" => 1,
+            "to" => 2,
+            "amount" => Account::find(1)->first()->balance + 100,
+            "details" => "test"
+        ];
+
+        $response = $this->json('POST', route('transactions.store', ['account', 1]), $data);
+        $response->assertStatus(400);
+        $response->assertJson(['success' => false]);
+    }
+
+    public function testGettingTransactions()
+    {
+        factory(Account::class, 2)->create();
+
+        $data = [
+            "from" => 1,
+            "to" => 2,
+            "amount" => Account::find(1)->first()->balance + 100,
+            "details" => "test"
+        ];
+
+        $this->json('POST', route('transactions.store', ['account' => 1]), $data);
+
+        $response = $this->json('GET', route('transactions.show', ['account' => 1]));
+
+        $response->assertStatus(200);
     }
 }
