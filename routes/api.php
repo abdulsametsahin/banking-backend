f3ed01e4d1cc3bffd289b11e8057bb5647d571df
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 22:43:59 +0300

    add route get getting transactions

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 Route::prefix('accounts')->group(function () {
     Route::get('{account}', 'AccountController@show')->name('accounts.show');
     Route::post('{account}/transactions', 'TransactionController@store')->name('transactions.store');
+    Route::get('{account}/transactions', 'TransactionController@show')->name('transactions.show');
 });
