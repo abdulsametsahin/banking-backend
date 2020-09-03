4d205f5f6eddeb63aed8549b7021d9ac0f6badc9
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 20:47:53 +0300

    create AccountController

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Account;
+use App\Helpers\Account\AccountFacade;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function show(Account $account)
+    {
+        return $this->makeResponse([
+            'account' => AccountFacade::getDTO($account)->toArray(),
+        ]);
+    }
 }
