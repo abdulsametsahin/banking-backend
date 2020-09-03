a7dbfef53b7e8916813ce590e13bcb48486aa464
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 20:56:58 +0300

    create transactions

---
+++ database/migrations/2020_09_03_175232_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from');
+            $table->unsignedBigInteger('to');
+            $table->decimal('amount', 9, 3);
+            $table->string('details');
+            $table->timestamps();
+
+            $table->foreign('from')->references('id')->on('accounts');
+            $table->foreign('to')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
