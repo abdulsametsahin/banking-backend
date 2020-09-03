770688b6a11097a039a6dd62fbde268e40490ff1
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-03 20:48:25 +0300

    create makeResponse method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function makeResponse(array $data, array $errors = [], int $status = 200): JsonResponse
+    {
+        return response()->json([
+            'data' => $data,
+            'errors' => $errors,
+            'success' => in_array($status, [200, 201])
+        ]);
+    }
 }
