9fa97f8d78c2e71e80bb009405afa6b2d07afb5d
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-05 02:18:53 +0300

    clean code & use strict types

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -1,14 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Account;
 use App\Helpers\Account\AccountFacade;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class AccountController extends Controller
 {
-    public function show(Account $account)
+    /**
+     * @param Account $account
+     * @return JsonResponse
+     */
+    public function show(Account $account): JsonResponse
     {
         return $this->makeResponse([
             'account' => AccountFacade::getDTO($account)->toArray(),
