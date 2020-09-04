03ac3d553cbf1931acad4d5421c6164f636a2149
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-05 02:19:38 +0300

    add properties

--- app/Transaction.php
+++ app/Transaction.php
@@ -4,6 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int id
+ * @property int from
+ * @property int to
+ * @property float amount
+ * @property string details
+ */
 class Transaction extends Model
 {
     //
