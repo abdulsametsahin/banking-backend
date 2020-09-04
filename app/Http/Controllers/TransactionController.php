25c8f52d8c4ad3b9541039664d219e8b5b8a73d6
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-05 02:19:55 +0300

    refactoring

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -1,24 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Account;
-use App\Helpers\Transaction\TransactionDTO;
 use App\Helpers\Transaction\TransactionFacade;
 use App\Http\Requests\TransactionStoreRequest;
 use Illuminate\Http\JsonResponse;
 
 class TransactionController extends Controller
 {
-    public function show(Account $account)
+    /**
+     * @param Account $account
+     * @return JsonResponse
+     */
+    public function show(Account $account): JsonResponse
     {
-        $transactions = array_map(function (TransactionDTO $transactionDTO) {
-            return $transactionDTO->toArray();
-        }, TransactionFacade::get($account));
-
-        return $this->makeResponse(compact('transactions'));
+        return $this->makeResponse([
+            'transactions' => TransactionFacade::getDTOs($account, true)
+        ]);
     }
 
+    /**
+     * @param Account $account
+     * @param TransactionStoreRequest $request
+     * @return JsonResponse
+     */
     public function store(Account $account, TransactionStoreRequest $request): JsonResponse
     {
         return TransactionFacade::save($request->denormalize()->toTransactionDTO($account));
